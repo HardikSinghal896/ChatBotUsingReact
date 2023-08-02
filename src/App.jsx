f6a4f8632cc5838b2dd5fbe2266227a692c255ea
Hardik Singhal <87109592+HardikSinghal896@users.noreply.github.com>
2023-08-02 23:42:09 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Box, Button, Container, HStack, Input, VStack } from "@chakra-ui/react"
 // import {Message} from "./Components/Message";
 import Message from "./Components/Message.jsx"
-import {onAuthStateChanged, getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { signOut, onAuthStateChanged, getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 import { app } from "./firebase"
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { query, orderBy, getFirestore, addDoc, collection, serverTimestamp, onSnapshot } from "firebase/firestore"
+
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 const loginHandler = () => {
   const provider = new GoogleAuthProvider();
@@ -12,36 +15,72 @@ const loginHandler = () => {
   signInWithPopup(auth, provider)
 }
 
+const logoutHandler = () => {
+  signOut(auth);
+}
+
 function App() {
   const [user, setUser] = useState(false);
-  <link rel="stylesheet" href="style.css" />
-  // <script src=""></script>
-  useEffect(()=>{
-    onAuthStateChanged(auth,(data)=>{
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const divforScroll = useRef(null);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log("fsdsadf");
+    try {
+      setMessage("");
+      await addDoc(collection(db, "Messages"), {
+        text: message,
+        uid: user.uid,
+        uri: user.photoURL,
+        createdAt: serverTimestamp(),
+      });
+      divforScroll.current.scrollIntoView({ behavior: "smooth" });
+
+    } catch (error) {
+      alert(error);
+    }
+  };
+  useEffect(() => {
+    const q = query(collection(db, "Messages"), orderBy("createdAt", "asc"));
+    const unsubscribe = onAuthStateChanged(auth, (data) => {
       // console.log(data);
       setUser(data);
     })
-  })
+    const unsubscribeForMessage = onSnapshot(q, (snap) => {
+      setMessages(
+        snap.docs.map((item) => {
+          const id = item.id;
+          return { id, ...item.data() };
+        })
+      );
+    });
+    return () => {
+      unsubscribe();
+      unsubscribeForMessage();
+    };
+  }, []);
 
   return (
     <Box bg={"red.50"}>
       {user ? (<Container h={"100vh"} bg={"white"}>
         <VStack h="full" >
-          <Button colorScheme={"red"} w={"full"} >Logout</Button>
+          <Button colorScheme={"red"} w={"full"} onClick={logoutHandler} >Logout</Button>
           <VStack h={"100vh"} w={"full"} overflowY="auto">
+            {
+              messages.map((item, key) => {
+                return <Message key={item.id} user={item.uid === user.uid ? "me" : "other"} text={item.text} uri={item.uri} />
+              })
+            }
 
-            <Message user="other" text={"Sample Message"} />
-            <Message user="other" text={"Sample Message"} />
-            <Message user="other" text={"Sample Message"} />
-            <Message user="other" text={"Sample Message"} />
-            <Message user="other" text={"Sample Message"} />
-            <Message user="other" text={"Sample Message"} />
-            <Message user="other" text={"Sample Message"} />
+            <div ref={divforScroll}></div>
           </VStack >
-          <form action="" style={{ width: "100%" }}>
+          <form onSubmit={submitHandler} style={{ width: "100%" }}>
             <HStack padding={"2"}>
-              <Input type="text" />
-              <Button bg={"purple.100"}>Send</Button>
+              <Input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+              <Button bg={"purple.100"} type={"submit"}>Send</Button>
             </HStack>
           </form>
         </VStack>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtK08TGGSRPzCHZRyhX1NKIbKXqGl18pU",
+  authDomain: "chatapphardiksinghal.firebaseapp.com",
+  projectId: "chatapphardiksinghal",
+  storageBucket: "chatapphardiksinghal.appspot.com",
+  messagingSenderId: "69842176785",
+  appId: "1:69842176785:web:f621905f9ac65ebf419ab0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
